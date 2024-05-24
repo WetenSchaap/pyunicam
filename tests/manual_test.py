7d95aeaf7978dcf698e27fdfa39d8fe8d296a787
WetenSchaap <git@swnkls.nl>
2024-05-24 13:39:52 +0200

    fix dependency error, add manual test

---
+++ tests/manual_test.py
@@ -0,0 +1,32 @@
+#%% imports
+import pyunicam
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+# %%
+c = pyunicam.connect_cam('thor')
+
+# %%
+d= c.takeOneImage()
+
+plt.imshow(d)
+print(f'max px value is {np.max(d)}')
+# %%
+print(c.getProperty('exposureTime'))
+# %%
+c.setProperty('exposureTime',100000)
+# %%
+d= c.takeOneImage()
+plt.imshow(d)
+
+print(f'max px value is {np.max(d)}')
+#%%
+c.startCapture()
+imgs = [ (c.getImages(),time.time()) for frame in range(20)] 
+c.stopCapture()
+# %%
+c.setProperty('acquisitionFramerate',0.5) 
+c.startCapture()
+imgs = [ (c.getImages(),time.time()) for frame in range(20)] 
+c.stopCapture()
+# %%
